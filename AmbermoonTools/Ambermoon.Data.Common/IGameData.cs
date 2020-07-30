c7440ff323ae0a2fc50e96de6be9c64ed9f862cb
Pyrdacor <trobt@web.de>
2020-07-30 10:55:09 +0200

    Added first version of the libs

---
+++ AmbermoonTools/Ambermoon.Data.Common/IGameData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Ambermoon.Data
+{
+    public interface IGameData
+    {
+        Dictionary<string, IFileContainer> Files { get; }
+
+        void Load(string folderPath);
+        Character2DAnimationInfo PlayerAnimationInfo { get; }
+    }
+}
---
+++ AmbermoonTools/Ambermoon.Data.Common/IMapReader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Ambermoon.Data
+{
+    public interface IMapReader
+    {
+        void ReadMap(Map map, IDataReader dataReader, IDataReader textDataReader, Dictionary<uint, Tileset> tilesets);
+    }
+}
