85b26769412318573b28a27e350c001a876ed471
Pyrdacor <trobt@web.de>
2020-08-08 00:30:14 +0200

    Adjusted render player interface

--- AmbermoonTools/Ambermoon.Data.Common/Render/IRenderPlayer.cs
+++ AmbermoonTools/Ambermoon.Data.Common/Render/IRenderPlayer.cs
@@ -5,7 +5,6 @@ namespace Ambermoon.Render
     public interface IRenderPlayer
     {
         Position Position { get; } // in Tiles
-        bool Move(int x, int y, uint ticks);
         void MoveTo(Map map, uint x, uint y, uint ticks, bool frameReset, CharacterDirection? newDirection);
     }
 }
