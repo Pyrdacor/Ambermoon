da5c0e690f09f78d58118518c88c9f145f67288d
Pyrdacor <trobt@web.de>
2020-08-12 15:10:45 +0200

    Improved executable loading/deploding

--- AmbermoonTools/Ambermoon.Data.Legacy/ItemManager.cs
+++ AmbermoonTools/Ambermoon.Data.Legacy/ItemManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Ambermoon.Data.Legacy
 {
@@ -11,15 +12,16 @@ namespace Ambermoon.Data.Legacy
             var file = gameData.Files[$"AM2_CPU"].Files[1];
 
             file.Position = 0;
-            var exploder = new ExecutableDeploder();
-            var data = exploder.Deplode(file);
+            var data = AmigaExecutable.Read(file).Last(h => h.Type == AmigaExecutable.HunkType.Data).Data;
             file = new DataReader(data);
 
+            // TODO: Test if the offset inside the data hunk is always the same (expecting 0xF8B4)
+
             // First find the item offset (the lamed ailment is the first item)
-            long offset = file.FindString("LAMED", 0x30000);
+            long offset = file.FindString("LAMED", 0);
 
             if (offset == -1)
-                offset = file.FindString("GELÄHMT", 0x30000);
+                offset = file.FindString("GELÄHMT", 0);
 
             if (offset == -1)
                 throw new AmbermoonException(ExceptionScope.Data, "Could not find item data in AM2_CPU.");
