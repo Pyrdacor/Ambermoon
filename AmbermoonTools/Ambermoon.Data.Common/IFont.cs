e23c379905463912409654d3c7bb292e1e3d40a2
Pyrdacor <trobt@web.de>
2020-09-09 13:19:02 +0200

    Removed nuget libs (were moved to Ambermoon.net)

--- AmbermoonTools/Ambermoon.Data.Common/IFont.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ambermoon.Data
-{
-    public interface IFont
-    {
-        Graphic GetGlyphGraphic(uint glyphIndex);
-    }
-}
--- AmbermoonTools/Ambermoon.Data.Common/IFontProvider.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ambermoon.Data
-{
-    public interface IFontProvider
-    {
-        IFont GetFont();
-    }
-}
