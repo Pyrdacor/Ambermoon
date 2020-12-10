3c6fea7eec721a85c6c9c52151dfd760afa01afb
Pyrdacor <trobt@web.de>
2020-12-10 09:49:53 +0100

    Excluded windows application from linux build

--- AmbermoonTools/pack.ps1
+++ AmbermoonTools/pack.ps1
@@ -7,9 +7,9 @@ if ($isWindows) {
   7z a AmbermoonTools-Windows.zip "AmbermoonTools\AmbermoonTextImport\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\AmbermoonTextImport.exe"
 } else {
   Write-Host Pack tar.gz for Linux
-  7z a AmbermoonTools-Linux.tar "./AmbermoonTools/AmbermoonPack/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/AmbermoonPack"
-  7z a AmbermoonTools-Linux.tar "./AmbermoonTools/MonsterValueChanger/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/MonsterValueChanger"
-  7z a AmbermoonTools-Linux.tar "./AmbermoonTools/AmbermoonTextImport/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/AmbermoonTextImport"
+  7z a AmbermoonTools-Linux.tar "./AmbermoonTools/AmbermoonPack/bin/Any CPU/ReleaseLinux/netcoreapp3.1/linux-x64/publish/AmbermoonPack"
+  7z a AmbermoonTools-Linux.tar "./AmbermoonTools/MonsterValueChanger/bin/Any CPU/ReleaseLinux/netcoreapp3.1/linux-x64/publish/MonsterValueChanger"
+  7z a AmbermoonTools-Linux.tar "./AmbermoonTools/AmbermoonTextImport/bin/Any CPU/ReleaseLinux/netcoreapp3.1/linux-x64/publish/AmbermoonTextImport"
   7z a AmbermoonTools-Linux.tar.gz AmbermoonTools-Linux.tar
   rm AmbermoonTools-Linux.tar
 }
