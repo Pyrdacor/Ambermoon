25e90cf5b420eb81df6ded5da21fc12211141f8f
Pyrdacor <trobt@web.de>
2020-12-11 06:09:14 +0100

    Removed subfolders from Windows zip

--- AmbermoonTools/pack.ps1
+++ AmbermoonTools/pack.ps1
@@ -2,9 +2,13 @@ $ErrorActionPreference = 'Stop';
 
 if ($isWindows) {
   Write-Host Pack zip for Windows
-  7z a AmbermoonTools-Windows.zip "AmbermoonTools\AmbermoonPack\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\AmbermoonPack.exe"
-  7z a AmbermoonTools-Windows.zip "AmbermoonTools\MonsterValueChanger\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\MonsterValueChanger.exe"
-  7z a AmbermoonTools-Windows.zip "AmbermoonTools\AmbermoonTextImport\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\AmbermoonTextImport.exe"
+  mkdir dist
+  copy "AmbermoonTools\AmbermoonPack\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\AmbermoonPack.exe" "dist\"
+  copy "AmbermoonTools\MonsterValueChanger\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\MonsterValueChanger.exe" "dist\"
+  copy "AmbermoonTools\AmbermoonTextImport\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\AmbermoonTextImport.exe" "dist\"
+  cd dist
+  7z a AmbermoonTools-Windows.zip *.*
+  rm -r dist
 } else {
   Write-Host Pack tar.gz for Linux
   7z a AmbermoonTools-Linux.tar "./AmbermoonTools/AmbermoonPack/bin/Any CPU/ReleaseLinux/netcoreapp3.1/linux-x64/publish/AmbermoonPack"
