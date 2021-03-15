8b7b080dbf30848caa3d6ab575a860b19593ad1f
Pyrdacor <trobt@web.de>
2021-03-15 14:53:51 +0100

    Reduced tool file sizes

--- AmbermoonTools/publish.ps1
+++ AmbermoonTools/publish.ps1
@@ -2,14 +2,14 @@ $ErrorActionPreference = 'Stop';
 
 if ($isWindows) {
   Write-Host Publish Windows executables
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonPack/AmbermoonPack.csproj" -p:PublishSingleFile=true -r win-x86 --no-restore
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonPack/AmbermoonPack.csproj" -p:PublishSingleFile=true -r win-x64 --no-restore
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/MonsterValueChanger/MonsterValueChanger.csproj" -p:PublishSingleFile=true -r win-x86 --no-restore
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/MonsterValueChanger/MonsterValueChanger.csproj" -p:PublishSingleFile=true -r win-x64 --no-restore
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonTextImport/AmbermoonTextImport.csproj" -p:PublishSingleFile=true -r win-x86 --no-restore
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonTextImport/AmbermoonTextImport.csproj" -p:PublishSingleFile=true -r win-x64 --no-restore
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonDiskExtract/AmbermoonDiskExtract.csproj" -p:PublishSingleFile=true -r win-x86 --no-restore
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonDiskExtract/AmbermoonDiskExtract.csproj" -p:PublishSingleFile=true -r win-x64 --no-restore
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonPack/AmbermoonPack.csproj" -p:PublishSingleFile=true -r win-x86 --no-restore --no-self-contained --nologo
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonPack/AmbermoonPack.csproj" -p:PublishSingleFile=true -r win-x64 --no-restore --no-self-contained --nologo
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/MonsterValueChanger/MonsterValueChanger.csproj" -p:PublishSingleFile=true -r win-x86 --no-restore --no-self-contained --nologo
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/MonsterValueChanger/MonsterValueChanger.csproj" -p:PublishSingleFile=true -r win-x64 --no-restore --no-self-contained --nologo
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonTextImport/AmbermoonTextImport.csproj" -p:PublishSingleFile=true -r win-x86 --no-restore --no-self-contained --nologo
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonTextImport/AmbermoonTextImport.csproj" -p:PublishSingleFile=true -r win-x64 --no-restore --no-self-contained --nologo
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonDiskExtract/AmbermoonDiskExtract.csproj" -p:PublishSingleFile=true -r win-x86 --no-restore --no-self-contained --nologo
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonDiskExtract/AmbermoonDiskExtract.csproj" -p:PublishSingleFile=true  -r win-x64 --no-restore --no-self-contained --nologo
   Write-Host Pack zip for Windows
   mkdir dist
   copy "AmbermoonTools\AmbermoonPack\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\AmbermoonPack.exe" "dist\"
@@ -30,10 +30,10 @@ if ($isWindows) {
   rm -r dist
 } else {
   Write-Host Publish Linux executable
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonPack/AmbermoonPack.csproj" -p:PublishSingleFile=true -r linux-x64 --no-restore
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/MonsterValueChanger/MonsterValueChanger.csproj" -p:PublishSingleFile=true -r linux-x64 --no-restore
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonTextImport/AmbermoonTextImport.csproj" -p:PublishSingleFile=true -r linux-x64 --no-restore
-  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonDiskExtract/AmbermoonDiskExtract.csproj" -p:PublishSingleFile=true -r linux-x64 --no-restore
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonPack/AmbermoonPack.csproj" -p:PublishSingleFile=true -r linux-x64 --no-restore --no-self-contained --nologo
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/MonsterValueChanger/MonsterValueChanger.csproj" -p:PublishSingleFile=true -r linux-x64 --no-restore --no-self-contained --nologo
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonTextImport/AmbermoonTextImport.csproj" -p:PublishSingleFile=true -r linux-x64 --no-restore --no-self-contained --nologo
+  dotnet publish -c $env:CONFIGURATION "./AmbermoonTools/AmbermoonDiskExtract/AmbermoonDiskExtract.csproj" -p:PublishSingleFile=true -r linux-x64 --no-restore --no-self-contained --nologo
   Write-Host Pack tar.gz for Linux
   7z a AmbermoonTools-Linux.tar "./AmbermoonTools/AmbermoonPack/bin/Any CPU/ReleaseLinux/netcoreapp3.1/linux-x64/publish/AmbermoonPack"
   7z a AmbermoonTools-Linux.tar "./AmbermoonTools/MonsterValueChanger/bin/Any CPU/ReleaseLinux/netcoreapp3.1/linux-x64/publish/MonsterValueChanger"
