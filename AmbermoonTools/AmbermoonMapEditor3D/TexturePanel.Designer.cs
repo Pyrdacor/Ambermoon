d03625507a87d6d4fb26a166a2ee0115e11589c5
Pyrdacor <trobt@web.de>
2022-06-03 12:56:23 +0200

    Some improvements

--- AmbermoonTools/AmbermoonMapEditor3D/TexturePanel.Designer.cs
+++ AmbermoonTools/AmbermoonMapEditor3D/TexturePanel.Designer.cs
@@ -50,7 +50,7 @@
             this.tabPageWalls.Location = new System.Drawing.Point(4, 24);
             this.tabPageWalls.Name = "tabPageWalls";
             this.tabPageWalls.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageWalls.Size = new System.Drawing.Size(732, 122);
+            this.tabPageWalls.Size = new System.Drawing.Size(732, 86);
             this.tabPageWalls.TabIndex = 0;
             this.tabPageWalls.Text = "Walls";
             this.tabPageWalls.UseVisualStyleBackColor = true;
@@ -60,7 +60,7 @@
             this.tabPageObjects.Location = new System.Drawing.Point(4, 24);
             this.tabPageObjects.Name = "tabPageObjects";
             this.tabPageObjects.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageObjects.Size = new System.Drawing.Size(732, 122);
+            this.tabPageObjects.Size = new System.Drawing.Size(732, 86);
             this.tabPageObjects.TabIndex = 1;
             this.tabPageObjects.Text = "Objects";
             this.tabPageObjects.UseVisualStyleBackColor = true;
--- AmbermoonTools/AmbermoonMapEditor3D/TexturePanel.cs
+++ AmbermoonTools/AmbermoonMapEditor3D/TexturePanel.cs
@@ -6,6 +6,8 @@
         private TexturePicker texturePickerObjects = new TexturePicker();
         int lastTexturePickerWallsHeight = 0;
         int lastTexturePickerObjectsHeight = 0;
+        int referenceHeightWalls = 0;
+        int referenceHeightObjects = 0;
 
         public TexturePanel(List<ITexturedObject> walls, List<ITexturedObject> objects)
         {
@@ -14,26 +16,28 @@
             texturePickerWalls.TexturedObjects = walls;
             texturePickerWalls.Dock = DockStyle.Fill;
             texturePickerWalls.Location = new Point(3, 3);
-            texturePickerWalls.Size = new Size(726, 116);
+            texturePickerWalls.Size = new Size(726, 80);
             texturePickerWalls.TabIndex = 0;
             texturePickerWalls.SizeChanged += TexturePickerWalls_SizeChanged;
 
             texturePickerObjects.TexturedObjects = objects;
             texturePickerObjects.Dock = DockStyle.Fill;
             texturePickerObjects.Location = new Point(3, 3);
-            texturePickerObjects.Size = new Size(726, 116);
+            texturePickerObjects.Size = new Size(726, 80);
             texturePickerObjects.TabIndex = 0;
             texturePickerObjects.SizeChanged += TexturePickerObjects_SizeChanged;
         }
 
         private void TexturePickerObjects_SizeChanged(object sender, EventArgs e)
         {
-            ResizeFit(ref lastTexturePickerObjectsHeight);
+            ResizeFit(ref lastTexturePickerObjectsHeight, referenceHeightObjects);
+            referenceHeightObjects = Height;
         }
 
         private void TexturePickerWalls_SizeChanged(object sender, EventArgs e)
         {
-            ResizeFit(ref lastTexturePickerWallsHeight);
+            ResizeFit(ref lastTexturePickerWallsHeight, referenceHeightWalls);
+            referenceHeightWalls = Height;
         }
 
         private void TexturePanel_Load(object sender, EventArgs e)
@@ -43,21 +47,32 @@
 
             lastTexturePickerWallsHeight = texturePickerWalls.Height;
             lastTexturePickerObjectsHeight = texturePickerObjects.Height;
+            referenceHeightWalls = Height;
+            referenceHeightObjects = Height;
+
+            texturePickerWalls.Height = 84;
         }
 
         private void TexturePanel_TabIndexChanged(object sender, EventArgs e)
         {
             int lastHeight = tabControl1.SelectedIndex == 0 ? lastTexturePickerWallsHeight : lastTexturePickerObjectsHeight;
+            int referenceHeight = tabControl1.SelectedIndex == 0 ? referenceHeightWalls : referenceHeightObjects;
 
-            ResizeFit(ref lastHeight);
+            ResizeFit(ref lastHeight, referenceHeight);
 
             if (tabControl1.SelectedIndex == 0)
+            {
                 lastTexturePickerWallsHeight = lastHeight;
+                referenceHeightWalls = Height;
+            }
             else
+            {
                 lastTexturePickerObjectsHeight = lastHeight;
+                referenceHeightObjects = Height;
+            }
         }
 
-        void ResizeFit(ref int lastHeight)
+        void ResizeFit(ref int lastHeight, int referenceHeight)
         {
             if (lastHeight == 0 || tabControl1?.SelectedTab?.Controls == null || tabControl1.SelectedTab.Controls.Count == 0)
                 return;
@@ -66,7 +81,8 @@
             int diff = height - lastHeight;
             lastHeight = height;
 
-            Height += diff;
+            MinimumSize = new Size(0, 0); // important!
+            Height = referenceHeight + diff;
             MinimumSize = new Size(0, Height);
         }
     }
