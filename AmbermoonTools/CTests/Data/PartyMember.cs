de868d75939954feab324742e95136da6f93f147
Pyrdacor <trobt@web.de>
2022-09-02 11:44:05 +0200

    Packer can now also unpack and pack items, added hex value changer

--- AmbermoonTools/CTests/Data/PartyMember.cs
+++ AmbermoonTools/CTests/Data/PartyMember.cs
@@ -5,6 +5,7 @@ namespace CSharpToC.Data
     [CExport]
     [CInherit(typeof(Character))]
     [CCodeForMethod(nameof(PartyMember), "this->Type = CharacterType_PartyMember;")]
+    [CSFileName($"{Program.BasePath}/data/character")]
     public class PartyMember : DataObject<PM>
     {
         public PartyMember() : base
