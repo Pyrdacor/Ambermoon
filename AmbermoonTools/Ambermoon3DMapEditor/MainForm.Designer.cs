5a098d35c388dbc5b75128495828136af22b22db
Pyrdacor <trobt@web.de>
2023-12-15 23:59:18 +0100

    3D map editor prototype

---
+++ AmbermoonTools/Ambermoon3DMapEditor/MainForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Ambermoon3DMapEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            splitContainer1 = new SplitContainer();
+            view2D = new Panel();
+            view3D = new OpenTK.WinForms.GLControl();
+            keyInputTimer = new System.Windows.Forms.Timer(components);
+            statusStrip1 = new StatusStrip();
+            statusPosition = new ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(view2D);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(view3D);
+            splitContainer1.Size = new Size(800, 450);
+            splitContainer1.SplitterDistance = 391;
+            splitContainer1.TabIndex = 0;
+            // 
+            // view2D
+            // 
+            view2D.Dock = DockStyle.Fill;
+            view2D.Location = new Point(0, 0);
+            view2D.Name = "view2D";
+            view2D.Size = new Size(391, 450);
+            view2D.TabIndex = 1;
+            // 
+            // view3D
+            // 
+            view3D.API = OpenTK.Windowing.Common.ContextAPI.OpenGL;
+            view3D.APIVersion = new Version(3, 3, 0, 0);
+            view3D.Dock = DockStyle.Fill;
+            view3D.Flags = OpenTK.Windowing.Common.ContextFlags.Default;
+            view3D.IsEventDriven = true;
+            view3D.Location = new Point(0, 0);
+            view3D.Name = "view3D";
+            view3D.Profile = OpenTK.Windowing.Common.ContextProfile.Compatability;
+            view3D.Size = new Size(405, 450);
+            view3D.TabIndex = 0;
+            view3D.Text = "glControl1";
+            view3D.Paint += view3D_Paint;
+            view3D.KeyDown += MainForm_KeyDown;
+            view3D.KeyUp += MainForm_KeyUp;
+            view3D.Resize += view3D_Resize;
+            // 
+            // keyInputTimer
+            // 
+            keyInputTimer.Enabled = true;
+            keyInputTimer.Interval = 20;
+            keyInputTimer.Tick += keyInputTimer_Tick;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { statusPosition });
+            statusStrip1.Location = new Point(0, 424);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 26);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // statusPosition
+            // 
+            statusPosition.Name = "statusPosition";
+            statusPosition.Size = new Size(29, 20);
+            statusPosition.Text = "X,Y";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip1);
+            Controls.Add(splitContainer1);
+            KeyPreview = true;
+            MinimumSize = new Size(640, 480);
+            Name = "MainForm";
+            Text = "Ambermoon Map Editor 3D";
+            Load += MainForm_Load;
+            KeyDown += MainForm_KeyDown;
+            KeyUp += MainForm_KeyUp;
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private OpenTK.WinForms.GLControl view3D;
+        private Panel view2D;
+        private System.Windows.Forms.Timer keyInputTimer;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel statusPosition;
+    }
+}
---
+++ AmbermoonTools/Ambermoon3DMapEditor/MainForm.cs
@@ -0,0 +1,228 @@
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.WinForms;
+
+namespace Ambermoon3DMapEditor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private const float BlockSize = 1.0f;
+        private float WallHeight = 2 * BlockSize / 3.0f;
+        private int MapWidth = 10;
+        private int MapHeight = 10;
+        private bool[] PressedKeys = Enumerable.Repeat(false, 256).ToArray();
+        private Matrix4 ModelViewMatrix = Matrix4.Identity;
+        private float PlayerX = 0.0f;
+        private float PlayerY = 0.0f;
+        private float PlayerViewAngle = 0.0f;
+        private const float MoveSpeed = 0.1f;
+        private const float TurnSpeed = 0.1f;
+
+        private void DrawFloor()
+        {
+            GL.Vertex3(0.0f, 0.0f, -MapHeight * BlockSize);
+            GL.Vertex3(MapWidth * BlockSize, 0.0f, -MapHeight * BlockSize);
+            GL.Vertex3(MapWidth * BlockSize, 0.0f, 0.0f);
+            GL.Vertex3(0.0f, 0.0f, 0.0f);
+        }
+
+        private void DrawCeiling()
+        {
+
+        }
+
+        private void DrawWall(int x, int y, Color4 color)
+        {
+            GL.Color4(color);
+
+            float left = x * BlockSize;
+            float right = left + BlockSize;
+            float far = -(MapHeight - y) * BlockSize;
+            float near = far + BlockSize;
+            float top = WallHeight;
+            float bottom = 0.0f;
+
+            // Front
+            GL.Vertex3(left, top, near);
+            GL.Vertex3(right, top, near);
+            GL.Vertex3(right, bottom, near);
+            GL.Vertex3(left, bottom, near);
+
+            // Left
+            GL.Vertex3(left, top, far);
+            GL.Vertex3(left, top, near);
+            GL.Vertex3(left, bottom, near);
+            GL.Vertex3(left, bottom, far);
+
+            // Back
+            GL.Vertex3(right, top, far);
+            GL.Vertex3(left, top, far);
+            GL.Vertex3(left, bottom, far);
+            GL.Vertex3(right, bottom, far);
+
+            // Right
+            GL.Vertex3(right, top, near);
+            GL.Vertex3(right, top, far);
+            GL.Vertex3(right, bottom, far);
+            GL.Vertex3(right, bottom, near);
+        }
+
+        private void view3D_Paint(object sender, PaintEventArgs e)
+        {
+            view3D.MakeCurrent();
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.Begin(BeginMode.Quads);
+
+            DrawFloor();
+            DrawCeiling();
+
+            DrawWall(0, 0, Color4.Red);
+            DrawWall(0, 2, Color4.Blue);
+            DrawWall(9, 8, Color4.Green);
+
+            GL.End();
+
+            /*
+            ... use various other GL.*() calls here to draw stuff ...
+            */
+
+            view3D.SwapBuffers();
+        }
+
+        private void view3D_Resize(object sender, EventArgs e)
+        {
+            view3D.MakeCurrent();
+
+            GL.Viewport(0, 0, view3D.ClientSize.Width, view3D.ClientSize.Height);
+
+            UpdateModelView();
+
+            Matrix4 perpective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, 1.6f, 1, 64);
+
+            GL.LoadMatrix(ref perpective);
+
+            /*
+                Usually you compute projection matrices here too, like this:
+
+                float aspect_ratio = MyGLControl.ClientSize.Width / (float)MyGLControl.ClientSize.Height;
+                Matrix4 perpective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspect_ratio, 1, 64);
+
+                And then you load that into OpenGL with a call like GL.LoadMatrix() or GL.Uniform().
+            */
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            PressedKeys[e.KeyValue] = true;
+        }
+
+        private void MainForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            PressedKeys[e.KeyValue] = false;
+        }
+
+        private void keyInputTimer_Tick(object sender, EventArgs e)
+        {
+            if (PressedKeys[(int)Keys.W])
+            {
+                if (PressedKeys[(int)Keys.Q])
+                {
+                    TurnLeft();
+                }
+                else if (PressedKeys[(int)Keys.E])
+                {
+                    TurnRight();
+                }
+
+                MoveForward();
+            }
+            else if (PressedKeys[(int)Keys.A])
+            {
+                MoveLeft();
+            }
+            else if (PressedKeys[(int)Keys.S])
+            {
+                MoveBackward();
+            }
+            else if (PressedKeys[(int)Keys.D])
+            {
+                MoveRight();
+            }
+            else if (PressedKeys[(int)Keys.Q])
+            {
+                TurnLeft();
+            }
+            else if (PressedKeys[(int)Keys.E])
+            {
+                TurnRight();
+            }
+        }
+
+        private void UpdateModelView()
+        {
+            view3D.MakeCurrent();
+            GL.MatrixMode(MatrixMode.Modelview);
+            ModelViewMatrix = Matrix4.CreateTranslation(-PlayerX, -0.5f * WallHeight, MapHeight * BlockSize - PlayerY);
+            ModelViewMatrix = Matrix4.Mult(ModelViewMatrix, Matrix4.CreateRotationY(PlayerViewAngle));
+            GL.LoadMatrix(ref ModelViewMatrix);
+            GL.MatrixMode(MatrixMode.Projection);
+            statusPosition.Text = $"X: {PlayerX:0.0}, Y: {PlayerY:0.0}";
+            Refresh();
+        }
+
+        private void MoveForward()
+        {
+            PlayerX += (float)(Math.Sin(PlayerViewAngle) * MoveSpeed);
+            PlayerY -= (float)(Math.Cos(PlayerViewAngle) * MoveSpeed);
+            UpdateModelView();
+        }
+
+        private void MoveBackward()
+        {
+            PlayerX -= (float)(Math.Sin(PlayerViewAngle) * MoveSpeed);
+            PlayerY += (float)(Math.Cos(PlayerViewAngle) * MoveSpeed);
+            UpdateModelView();
+        }
+
+        private void MoveLeft()
+        {
+            PlayerX -= (float)(Math.Cos(PlayerViewAngle) * MoveSpeed);
+            PlayerY -= (float)(Math.Sin(PlayerViewAngle) * MoveSpeed);
+            UpdateModelView();
+        }
+
+        private void MoveRight()
+        {
+            PlayerX += (float)(Math.Cos(PlayerViewAngle) * MoveSpeed);
+            PlayerY += (float)(Math.Sin(PlayerViewAngle) * MoveSpeed);
+            UpdateModelView();
+        }
+
+        private void TurnLeft()
+        {
+            PlayerViewAngle -= TurnSpeed;
+            if (PlayerViewAngle < 0.0f)
+                PlayerViewAngle += MathHelper.TwoPi;
+            UpdateModelView();
+        }
+
+        private void TurnRight()
+        {
+            PlayerViewAngle += TurnSpeed;
+            if (PlayerViewAngle >= MathHelper.TwoPi)
+                PlayerViewAngle -= MathHelper.TwoPi;
+            UpdateModelView();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            GL.Enable(EnableCap.DepthTest);
+        }
+    }
+}
