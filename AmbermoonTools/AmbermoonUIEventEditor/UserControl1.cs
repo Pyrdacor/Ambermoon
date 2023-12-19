9cd98ce32db89bbb05d59e2fe0f7a82b9368d2d7
Pyrdacor <trobt@web.de>
2023-12-19 10:52:21 +0100

    Preparation for UI event editor

---
+++ AmbermoonTools/AmbermoonUIEventEditor/UserControl1.cs
@@ -0,0 +1,10 @@
+﻿namespace AmbermoonUIEventEditor
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
