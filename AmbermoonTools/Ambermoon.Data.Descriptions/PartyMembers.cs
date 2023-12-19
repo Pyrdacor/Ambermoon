56831af62a2a17abda73cb737d7e2649551d9eaa
Pyrdacor <trobt@web.de>
2023-12-19 10:48:54 +0100

    Added missing file

---
+++ AmbermoonTools/Ambermoon.Data.Descriptions/PartyMembers.cs
@@ -0,0 +1,23 @@
+﻿namespace Ambermoon.Data.Descriptions
+{
+    public enum PartyMembers
+    {
+        None,
+        Hero,
+        Netsrak,
+        Mando,
+        Erik,
+        Chris,
+        Monika,
+        Tar,
+        Egil,
+        Selena,
+        Nelvin,
+        Sabine,
+        Valdyn,
+        Targor,
+        Leonaria,
+        Gryban,
+        Kasimir,
+    }
+}
