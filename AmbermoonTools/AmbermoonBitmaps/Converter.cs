cfe73e75cb74bbe42a85533fbe0a669bda575911
Pyrdacor <trobt@web.de>
2025-06-29 18:51:18 +0200

    Added lib prototype for converting Ambermoon graphics to Bitmaps

---
+++ AmbermoonTools/AmbermoonBitmaps/Converter.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using Ambermoon.Data;
+
+namespace AmbermoonBitmaps;
+
+public static class Converter
+{
+    public static Bitmap GraphicToBitmap(Graphic graphic, Graphic palette) => GraphicToBitmap(graphic, new PaletteColors(palette));
+
+    public static Bitmap GraphicToBitmap(Graphic graphic, PaletteColors palette, int? colorKeyIndex = 0)
+    {
+        var bitmap = new Bitmap(graphic.Width, graphic.Height);
+        var data = bitmap.LockBits(new(0, 0, graphic.Width, graphic.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        int sourceIndex = 0;
+        int targetIndex = 0;
+        var targetData = new byte[graphic.Width * graphic.Height * 4];
+
+        for (int y = 0; y < graphic.Height; y++)
+        {
+            for (int x = 0; x < graphic.Width; x++)
+            {
+                int colorIndex = graphic.Data[sourceIndex++];
+
+                if (colorKeyIndex == colorIndex)
+                {
+                    targetData[targetIndex++] = 0; // Blue
+                    targetData[targetIndex++] = 0; // Green
+                    targetData[targetIndex++] = 0; // Red
+                    targetData[targetIndex++] = 0; // Alpha
+                }
+                else
+                {
+                    var color = palette.Colors[colorIndex];
+                    targetData[targetIndex++] = color.B; // Blue
+                    targetData[targetIndex++] = color.G; // Green
+                    targetData[targetIndex++] = color.R; // Red
+                    targetData[targetIndex++] = color.A; // Alpha
+                }
+            }
+        }
+
+        Marshal.Copy(targetData, 0, data.Scan0, targetData.Length);
+
+        bitmap.UnlockBits(data);
+
+        return bitmap;
+    }
+}
---
+++ AmbermoonTools/AmbermoonBitmaps/PaletteColors.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using Ambermoon.Data;
+
+namespace AmbermoonBitmaps;
+
+public class PaletteColors
+{
+    public readonly Color[] Colors = new Color[32];
+
+    public PaletteColors()
+    {
+
+    }
+
+    public PaletteColors(Graphic palette)
+    {
+        for (int i = 0; i < Colors.Length; i++)
+        {
+            var r = palette.Data[i * 2] & 0xf;
+            var gb = palette.Data[i * 2 + 1];
+            var g = gb >> 4;
+            var b = gb & 0xf;
+
+            r |= (r << 4);
+            g |= (g << 4);
+            b |= (b << 4);
+
+            Colors[i] = Color.FromArgb(255, r, g, b);
+        }
+    }
+}
